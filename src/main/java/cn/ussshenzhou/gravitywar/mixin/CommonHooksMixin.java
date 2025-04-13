990b607b7ea934411abf656078af22f06a08bd1b
USS-Shenzhou <969840795@qq.com>
2025-04-13 13:08:19 +0800

    remap, wtf?

---
+++ src/main/java/cn/ussshenzhou/gravitywar/mixin/CommonHooksMixin.java
@@ -0,0 +1,28 @@
+package cn.ussshenzhou.gravitywar.mixin;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import gravity_changer.api.GravityChangerAPI;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.neoforged.neoforge.common.CommonHooks;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+/**
+ * @author USS_Shenzhou
+ */
+@Mixin(CommonHooks.class)
+public class CommonHooksMixin {
+
+    @Redirect(method = "onLivingBreathe",at = @At(value = "INVOKE", target = "Lnet/minecraft/core/BlockPos;containing(DDD)Lnet/minecraft/core/BlockPos;"))
+    private static BlockPos gravityChangerCompat(double x, double y, double z, @Local(argsOnly = true) LivingEntity entity) {
+        Direction gravityDirection = GravityChangerAPI.getGravityDirection(entity);
+        if (gravityDirection == Direction.DOWN) {
+            return BlockPos.containing(x, y, z);
+        }
+        return BlockPos.containing(entity.getEyePosition());
+    }
+}
