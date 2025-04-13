14b07897e0455391f9c53fb841ef649f1ad2c586
USS-Shenzhou <969840795@qq.com>
2025-04-13 11:26:54 +0800

    first commit

---
+++ src/main/java/cn/ussshenzhou/gravitywar/Gravitywar.java
@@ -0,0 +1,13 @@
+package cn.ussshenzhou.gravitywar;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(Gravitywar.MODID)
+public class Gravitywar {
+    public static final String MODID = "gravitywar";
+
+    public Gravitywar(IEventBus modEventBus, ModContainer modContainer) {
+    }
+}
